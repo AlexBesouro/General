b95c282f59939ea65c5f3b4aa069e69b7a02e2f0
Aleks <aleksbesouro@gmail.com>
2025-12-12 14:01:41 +0100

    first commit

---
+++ venv_example/main.py
@@ -0,0 +1,15 @@
+"Commands i used"
+
+"python3 -m -venv <name> (almost always venv)" # command to create a virtual environment 
+# python3 - its a launcher for python
+# -3 - specifies to use python 3
+# -m - tells python to run a module as a script
+# -venv - the module to create virtual environments 
+# <name> - the name of the virtual environment folder
+# rm -rf venv # command to delete a virtual environment folder
+
+"source <name>/bin/activate" # command to activate the virtual environment
+# source - a shell built-in command to run commands from a file in the current shell
+# <name>/bin/activate - the path to the activate script inside the virtual environment folder
+# deactivate # command to deactivate the virtual environment
+
